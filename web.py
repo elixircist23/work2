2a49118c3ec51d9c2bcd5e1b3490623a928aba71
elixircist23 <elixircist@gmail.com>
2016-08-31 23:56:01 -0400

    added endpoints
    handlers for main, insert, and show along with their respective
    get/posts, INSERT AND SHOW WORKING

--- web.py
+++ web.py
@@ -2,19 +2,36 @@ import tornado.httpserver
 import tornado.ioloop
 import tornado.options
 import tornado.web
+import json
 from dbutil import DBUtil
 
 class Application(tornado.web.RequestHandler):
 	def get(self):
-		db = DBUtil()
-		data = db.jason()
+		self.render('insert.html')
+		
+class InsertHandler(tornado.web.RequestHandler):
+	def get(self):
+		self.write('testing')
+	def post(self):
+		name = self.get_argument('name')
+		age = int(self.get_argument('age'))
+		city = self.get_argument('city')
+		
+		db = DBUtil('Info.db', 'information')
+		db.insert(name, age, city)
 		db.close()
-		self.write(data)	
+		
+class GetHandler(tornado.web.RequestHandler):
+	def get(self):
+		db = DBUtil('Info.db', 'information')
+		data = json.dumps(db.getUsers())
+		self.write(data)
+		db.close()
 	
 	
 
 if __name__ == '__main__':
-	app = tornado.web.Application(handlers =[(r'/', Application)])
+	app = tornado.web.Application(handlers =[(r'/', Application), (r'/insert', InsertHandler), (r'/show', GetHandler)])
 	http_server = tornado.httpserver.HTTPServer(app)
 	http_server.listen(8888)
 	tornado.ioloop.IOLoop.instance().start()
